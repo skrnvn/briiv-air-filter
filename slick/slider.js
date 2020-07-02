1c6c03b635de06cfcb8fcf6aec900c2c6f23e1fa
skrnvn <skrnvn@gmail.com>
2020-07-02 13:33:14 +0300

    add slider

---
+++ slick/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.regular').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+    })
+})
