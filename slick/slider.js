07e3d80c608e47f1996902fb1642dfb02dca1063
skrnvn <skrnvn@gmail.com>
2020-07-03 12:29:42 +0300

    finish main markup

--- slick/slider.js
+++ slick/slider.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
-    $('.regular').slick({
+    $('.regular--small').slick({
         dots: true,
         infinite: true,
         slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+    })
+})
+
+$(document).ready(function() {
+    $('.regular--large').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
         slidesToScroll: 1,
         variableWidth: true,
     })
